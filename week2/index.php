88c091fe1067970782add781b839b7696f699fba
Samuel Owusu <samuelnii6000@gmail.com>
2025-05-14 19:59:11 -0400

    week 2 variables

---
+++ week2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Hello world
+    </h1>
+
+        <?php
+        $fname = "Samuel";
+        $lname = "Owusu";
+
+        echo "<h1>Hello Hello</h1>";
+        echo "Hello " . $fname . " " . $lname;
+        ?>
+    
+</body>
+</html>
